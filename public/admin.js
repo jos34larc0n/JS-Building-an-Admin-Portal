0bb5ee59395411558583359fbec827b3ddfbd82e
JOSE ALARCON MOLINA <jos3.4larc0n@gmail.com>
2023-01-25 01:20:14 -0500

    final commit, important from git warning: in the working copy of 'db.json', LF will be replaced by CRLF the next time Git touches it

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,53 @@
+//My code
+//get all the books
+//book, title, input, button
+//add eventListener to save button to make request to update book
+// fetch request
 
-// Your Code Here
+async function main(){
+    // Obtain a list of books from the server
+    const response = await fetch('http://localhost:3001/listBooks')
+    // Parse the obtained list of books
+    const books = await response.json()
+    // Iterate over the list of books and display each book
+    books.forEach(renderBook)
+}
+
+function renderBook(book){
+    console.log(book)
+    const root = document.getElementById('root')
+    const li = document.createElement('li')
+    li.textContent = book.title
+    const input = document.createElement('input')
+    // Set the value of the element to the book quantity
+    input.value = book.quantity
+    // Add an event listener to the element, to update the book quantity 
+    input.addEventListener('change', (event) => {
+        book.quantity = event.target.value;
+  });
+  // Create an element to save the book quantity
+  const saveButton = document.createElement('button')
+  saveButton.textContent = 'Save'
+  // Add an event listener to the element, to save the book quantity to the server when clicked
+  saveButton.addEventListener('click', () => {
+      // Create a request body with the book id and updated quantity
+      const body = {
+          id: book.id,
+          quantity: input.value
+       }
+      // Send a request to update the book on the server
+      fetch('http://localhost:3001/updateBook', {
+          method: 'PATCH',
+          headers: {
+              'content-type' : 'application/json'
+          },
+          body: JSON.stringify(body)
+        })
+   })
+  li.appendChild(input)
+  li.appendChild(saveButton)
+  root.appendChild(li) 
+}
+
+// Call the main function to start the application
+main()
